53e8333d85001848af9c128aeedb616103251553
okogikam <okogikam@gmail.com>
2024-12-17 22:16:40 +0800

    update test php

--- hantube/test.php
+++ hantube/test.php
@@ -14,10 +14,19 @@ $conn = mysqli_connect($hostname,$username,$password,$database);
 if(!$conn){
     $hasil['status'] = mysqli_connect_errno();
     $hasil['result'] = "Error : ". mysqli_connect_error();    
-}else if(isset($_GET['un']) && isset($_GET['ps'])){
+}else if(isset($_GET['type']) && isset($_GET['un']) && isset($_GET['ps'])){
     $un = $_GET['un'];
-    $ps = $_GET['ps'];    
-    $hasil['result'] = getData($un,$ps);
+    $ps = $_GET['ps'];  
+    switch($_GET['type']){
+        case "1":
+            $pl = $_GET['pl'];
+            $hs = $_GET['hs'];
+            newUser($un,$ps,$pl,$hs);
+            break;
+        default :
+            $hasil['result'] = getData($un,$ps);
+            break; 
+    }  
 }else{
     $hasil['status'] = "300";
 }
@@ -26,11 +35,24 @@ echo json_encode($hasil);
 
 function getData($un,$ps){
     global $conn, $hasil;
-    $query = "SELECT * FROM hantub WHERE username='$un'AND password='$ps'";
+    $query = "SELECT * FROM hantub_user WHERE username='$un'AND password='$ps'";
     $result = mysqli_query($conn,$query);
     if($result->num_rows > 0){
         $hasil['status'] = "200";
         $data = mysqli_fetch_assoc($result);
+    }else{
+        $hasil['status'] = "205";
+        $data = "";
+    }
+    return $data;
+}
+function newUser($un,$ps,$pl,$hs){
+    global $conn, $hasil;
+    $query = "INSERT INTO hantub_user(username,password,playlist,history) VALUES('$un','$ps','$pl','$hs')";
+    $result = mysqli_query($conn,$query);
+    if($result){
+        $hasil['status'] = "200";
+        $data = $result;
     }else{
         $hasil['status'] = "205";
         $data = "";
